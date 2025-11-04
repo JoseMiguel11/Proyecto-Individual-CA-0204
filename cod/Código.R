48fb52bde0b04b0928859962b229fb8ca15ca7a0
José Miguel Rodríguez Gómez <jose.rodriguezgomez@ucr.ac.cr>
2025-11-03 22:09:03 -0600

    refactor: Se cambió la forma en que se leen los archivos, ahora se leen desde Dropbox y no desde el local.

--- cod/Código.R
+++ cod/Código.R
@@ -4,11 +4,27 @@ library(sf)
 library(dplyr)
 library(ggplot2)
 
+# Link de la carpeta en dropbox
+url <- "https://www.dropbox.com/scl/fi/z1npogjqf3q6lohm4g3f3/MAMMALS.zip?rlkey=zzia7ftshjnzwef7ovfqiytfn&st=beetnd2f&dl=1"
 
-# Importar las bases de datos
-mammals.1 <- st_read("C:/Users/mr214/Documents/Herramientas de ciencias de Datos I/Proyecto Individual/data/MAMMALS/MAMMALS_PART1.shp")
-mammals.2 <- st_read("C:/Users/mr214/Documents/Herramientas de ciencias de Datos I/Proyecto Individual/data/MAMMALS/MAMMALS_PART2.shp")
+# Crear carpeta data en caso de que no exista, si ya existe no pasará nada, solo que no se creará
+dir.create("data", recursive = TRUE, showWarnings = FALSE)
 
+# Descargar el archivo.zip, si ya existe en la ubicación entonces no lo descargará
+if (!file.exists("data/MAMMALS.zip")) {
+  options(timeout = 600)  # Tiempo máximo para la descarga, puse 600 segundos, 10 minutos
+  download.file(url, "data/MAMMALS.zip", mode = "wb")
+} 
+
+# Descomprimir la carpeta MAMMALS en data
+unzip("data/MAMMALS.zip", exdir = "data")
+
+# Buscar los archivos shapefile, con extención .shp y los guardamos en archivos.shp
+archivos.shp <- list.files("data", pattern = "\\.shp$", recursive = TRUE, full.names = TRUE)
+
+# Leer los shapefiles
+mammals.1 <- st_read(archivos.shp[1]) # Primera parte de los datos
+mammals.2 <- st_read(archivos.shp[2]) # Segunda parte de los datos
 
 # Transformar los datos a tipo data frame
 datos.1 <- as.data.frame(mammals.1) |> 
