483064541498a3cf98e8dd0d666283827fa51e1e
Joseee <josemiguelrg119@gmail.com>
2025-12-03 23:48:21 -0600

    style, feat: se cambió el gráfico de la distribución mundial de las categorías, ahora está ordenado para que las capas más altas sean las más recientes. También se guardaron los gráficos en la carpeta res

--- cod/Código.R
+++ cod/Código.R
@@ -34,11 +34,6 @@ mammals.unido <- rbind(mammals.1, mammals.2)
 unique(mammals.1$category)
 
 
-# Data frame con la observación más reciente
-datos.unicos <- datos |> 
-  arrange(desc(yrcompiled)) |>  # Ordenar por año más reciente
-  distinct(sci_name, .keep_all = TRUE) # Seleccionar por cada nombre científico, solo la primer observación (Como está ordenado a más reciente entonces toma la observación más reciente)
-
 # Transformar los datos a tipo data frame
 datos.1 <- as.data.frame(mammals.1) |> 
   select(-geometry)
@@ -47,6 +42,12 @@ datos.2 <- as.data.frame(mammals.2) |>
 
 # Unir ambos data frames en uno  
 datos <- rbind(datos.1, datos.2)
+
+# Data frame con la observación más reciente
+datos.unicos <- datos |> 
+  arrange(desc(yrcompiled)) |>  # Ordenar por año más reciente
+  distinct(sci_name, .keep_all = TRUE) # Seleccionar por cada nombre científico, solo la primer observación (Como está ordenado a más reciente entonces toma la observación más reciente)
+
 
 # Verificar los nombres de las variables y sus tipos de datos
 str(datos)
@@ -117,13 +118,13 @@ ggplot(aes(x = reorder(habitat, porcentaje), y = porcentaje, fill = habitat)) +
   theme_minimal() +
   theme(legend.position = "none") +
   coord_flip()
-ggsave("res/distirbucion.habitat.pdf", plot = grafico.2, width = 6, height = 5)
-ggsave("res/distirbucion.habitat.png", plot = grafico.2, width = 6, height = 5)
+ggsave("res/distribucion.habitat.pdf", plot = grafico.2, width = 6, height = 5)
+ggsave("res/distribucion.habitat.png", plot = grafico.2, width = 6, height = 5)
 
 
 
-# Gráfico sobre la Distribución de vulnerabilidad global de las especies estudiadas
-mammals.1 |> 
+# Gráfico sobre la Distribución de vulnerabilidad global de las especies estudiadas para la base de datos 1
+grafico.3 <- mammals.1 |> 
   ggplot() +
   geom_sf(aes(fill = category), alpha = 0.7, size = 0.1) +
   scale_fill_manual(values = c("EX" = "black", "EW" = "#23DBDB", "CR" = "red", "EN" = "orange", 
@@ -138,14 +139,42 @@ mammals.1 |>
                                "DD" = "Datos Insuficientes",
                                "NE" = "No Evaluado")) +  # Cambiar abreviación por palabras en el código de colores
   labs(title = "Distribución Global de Especies Amenazadas") +
-  theme_void() # Borrar las coodenadas, fondos, nombres de los ejes, etc
+  theme_void() # Borrar las coordenadas, fondos, nombres de los ejes, etc
+
+ggsave("res/distribucion.mundial.categorías.1.pdf", plot = grafico.3, width = 8, height = 5)
+ggsave("res/distribucion.mundial.categorías.1.png", plot = grafico.3, width = 8, height = 5)
 
 
-#Crear el mapa mundi
+mammals.ordenado <- mammals.unido |> 
+  arrange(yrcompiled)
+  
+# Gráfico sobre la distribución global del estado de las especies investigadas, ordenado de más antiguo a más reciente
+grafico.4 <- mammals.ordenado |> 
+  ggplot() +
+  geom_sf(aes(fill = category), alpha = 0.7, size = 0.1) +
+  scale_fill_manual(values = c("EX" = "black", "EW" = "#23DBDB", "CR" = "red", "EN" = "orange", 
+                               "VU" = "yellow", "NT" = "lightblue", "LC" = "green", "DD" = "grey", "NE" = "white"), # Poner los colores manualmente
+                    labels = c("EX" = "Extinto",
+                               "EW" = "Extinto en Estado Silvestre",
+                               "CR" = "En Peligro Crítico",
+                               "EN" = "En Peligro",
+                               "VU" = "Vulnerable",
+                               "NT" = "Casi Amenazado",
+                               "LC" = "Preocupación Menor",
+                               "DD" = "Datos Insuficientes",
+                               "NE" = "No Evaluado")) + 
+  labs(title = "Distribución Global de Especies Amenazadas") +
+  theme_void()
+
+ggsave("res/distribucion.mundial.categorías.pdf", plot = grafico.4, width = 8, height = 5)
+ggsave("res/distribucion.mundial.categorías.png", plot = grafico.4, width = 8, height = 5)
+
+
+#Crear el mapamundi
 mapa.mundi <- ne_countries(scale = "medium", returnclass = "sf")
 
 # Gráfico sobre la Distribución de especies en estado crítico
-mammals.unido |> 
+grafico.5 <- mammals.unido |> 
   filter(category == "CR") |> 
   ggplot() +
   geom_sf(data = mapa.mundi, fill = "white", color = "gray", size = 0.2) +
@@ -160,13 +189,17 @@ mammals.unido |>
                                "NT" = "Casi Amenazado",
                                "LC" = "Preocupación Menor",
                                "DD" = "Datos Insuficientes",
-                               "NE" = "No Evaluado")) +  # Cambiar abreviación por palabras en el código de colores
+                               "NE" = "No Evaluado")) +  
   labs(title = "Distribución Global de Especies en Categoría Crítica",
        caption = "Fuente: IUCN Red List of Threatened Species",
        fill = "Categoría") +
-  theme_void() # Borrar las coodenadas, fondos, nombres de los ejes, etc
+  theme_void() 
 
-# Códigos devulnerabilidad
+ggsave("res/distribucion.mundial.críticas.pdf", plot = grafico.5, width = 8, height = 5)
+ggsave("res/distribucion.mundial.críticas.png", plot = grafico.5, width = 8, height = 5)
+
+
+# Códigos de vulnerabilidad
 # EX: Extinto
 # EW: Extinto en estado silvestre
 # CR: En peligro crítico
@@ -190,7 +223,7 @@ frecuencia.amenaza.por.categoria <- datos.unicos |>
   filter(n() == 3) |> # Seleccionar solamente los que tienen las 3 categorías
   ungroup() |> 
   arrange(desc(total.amenazadas))  |> 
-  head(36) # 12 órdenes taxonomicos (Se pone 36 como parámetro pues hay 3 filas por cada orden)
+  head(36) # 12 órdenes taxonómicos (Se pone 36 como parámetro pues hay 3 filas por cada orden)
 # Solo se escogieron 12 órdenes taxonómicos pues solo hay 12 grupos con las 3 categorías
 
 
